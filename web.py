a879c1cfb536b6e1cee1e85c3f2f452cc01c7145
krzysztofmrozik <krzysm20009042@gmail.com>
2024-08-14 16:34:46 +0200

    Implemented action on checkbox click

--- web.py
+++ web.py
@@ -12,11 +12,15 @@ st.title('My todo list')
 st.subheader('Lets get to work')
 st.write('This app is to increase my productivity')
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label='-', placeholder='Add new todo...',
               on_change=add_todo, key='new_todo')
 
-st.session_state
 
